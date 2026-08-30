6fe9e999c01bc9b0b73c2b41d911bbbd9cfbeb1f
Ashwendra <iamshivayraj@gmail.com>
2026-08-30 15:30:49 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,454 @@
+#include<iostream>
+using namespace std;
+// // is used for comment
+//this code run by ashwin 
+//this
+//is multiple 
+//line code
+//main(){
+	//int sum=6;
+	
+	
+//	cout<< "hello world"<< sum ;
+//}
+// concept of global(do work )
+# include<iostream>
+//main(){
+////	std::cout<<"this is my code";
+//	
+////}
+//# include<iostream>
+//using namespace std;
+//int global=6;
+//void sum(){
+//	int a;
+//}
+//
+//main(){
+//	int global=8;
+//	global=78;
+//	int a=15;
+//	int b=23;
+//	float pi=3.14;
+//	float sigma=2.34;
+//	sum();
+	//cout<<global; 
+//	cout<<" this is number . here the value of a is "<<a<<".the value of b is " <<b;
+	//cout<<"this is number here the value of sigma is "<<sigma<<"the value of pi is "<<pi;
+//}
+//#include<iostream>
+//using namespace std;
+//int a=5;
+//void display(){
+//	int a=10;
+//	cout<<a;
+//	cout<<a;
+//	
+//}
+//main(){
+//	display();
+//}
+// user input and output
+//# include <iostream>
+//using namespace std;
+//int main(){
+//	int num1,num2;
+//	cout<<"enter the value of num1:: "; // this is called insertion operator  << 
+//	cin>>num1; // this is called extraction operator >>
+//	cout<<"enter the value of num2:: ";
+//	cin>>num2;
+//	cout<< "the sum is "<< num1+num2;
+//	
+//	
+//}
+// header file and operator 
+// there are two types of header files
+// 1. system header file it comes with the compiler  #include<iostream>
+//2. user defined header file it is written by the programmer 
+// #inlcude"hello.h" this file must be present 
+//#include"hello"
+// arithmetic operator 
+//#include<iostream>
+//using namespace std;
+//main(){
+//	int a=4,b=5;
+//	cout<<"the value of  a+b is "<<a+b<<endl;
+//	cout<<"the value of a-b is  "<<a-b<<endl;
+//	cout<<"the value of a*b is  "<<a*b<<endl;
+//	cout<<"the value of a/b is "<<a/b<<endl;
+//	cout<<"the value of a%b is"<<a%b<<endl;
+//	cout<<"the value of a++ is "<<a++<<endl;
+//	cout<<"the value of a-- is "<<a--<<endl;
+//	cout<<"the value of ++a is "<<++a<<endl;
+//	cout<<" the value of --a is "<<--a<<endl;
+//	// assignment operator
+////	int a=34,b=56;
+////	char d="ashish"
+//// comparison operator
+//  cout<< " the value of a==b is "<< (a==b)<<endl;
+//  cout<<"the value of a!=b is "<<(a!=b)<<endl;
+//  cout<<"the value of a>=b is "<<(a>=b)<<endl;
+//  cout<<"the value of a<= is "<<(a<=b)<<endl;
+//  cout<<"the value of a>b u=is  "<<(a>b)<<endl;
+//   cout<<"the value of a<b is "<<(a<b)<<endl;
+//   // logical operator 
+//   cout<< "the value of logical operator (a==b) && (a<b) is "<<((a==b) && (a<b))<<endl;
+//   cout<< "the value of logical operator (a==b) || (a<b) is "<<((a==b) || (a<b))<<endl;
+//   cout<< "the value of logical operator(!(a==b) is "<<(!(a==b))<<endl;
+//   
+//   
+//
+//	
+//}
+
+//#include<iostream>
+//using namespace std;
+//int c=49;
+//main(){
+//	int a,b,c ;
+//	cout<<"the value of a is "<<endl;
+//	cin>>a;
+//	cout<<"the value of b is"<<endl;
+//	cin>>b;
+//	c=a+b;
+//	cout<<"the sum is "<<c<<endl;
+//	cout<<"the global value is "<<::c;
+//	float d=34.5;
+//	long double e=34.5;
+//	cout<<"the size of 34.5 is "<<sizeof(34.5)<<endl;
+//	cout<<"the size of 34.5f is "<<sizeof(34.5f)<<endl;
+//	cout<<"the size of 34.5F is "<<sizeof(34.5F)<<endl;
+//	cout<<"the size of 34.5l is "<<sizeof(34.5l)<<endl;
+//	cout<<"the size of 34.5L is "<<sizeof(34.5L)<<endl;
+//	cout<<"the value of d is"<<d<<endl<<"the value of e is"<<e;
+//	// reference variable
+//	float f=455;
+//	float & g=f;
+//	//cout<<f<<endl;
+//	cout<<g<<endl;
+	// typecasting
+//	float a=45;
+//	float b=45.45;
+//	cout<<"the value of a is" <<(int)a<<endl;
+//	cout<<"the value of a is"<<int(a)<<endl;
+//	cout<<"the value of b is "<<int(b)<<endl;
+//	cout<<"the value of b is "<<(int)b<<endl;
+//	cout<<"the value of a+b is "<<a+b<<endl;
+//	cout<<"the value of a+int(b) is "<<a+int(b)<<endl;
+//	cout<<"the value of a+(int)b is "<<a+(int)b<<endl;
+//	
+//	
+//}
+// constant and manipulators
+//#include<iostream>
+//#include<iomanip>
+//using namespace std;
+//main(){
+	//int a=4;
+	//cout<<"the value of a was "<<a<<endl;
+//	a=45;
+//	cout<<"the value of a is "<<a<<endl;
+//	const int  b=3;
+//	cout<<"the value of b was"<<b<<endl;
+//	b=60;
+//	cout<<"the value of b is "<<b<<endl;
+//
+//int a =3,b=57,c=43;
+//cout<<"the value of a is "<<a<<endl;
+//cout<<"the value of b is "<<b<<endl;
+//cout<<"the value of c is "<<c<<endl;
+//
+//cout<<"the value of a is "<<setw(4)<<a<<endl;
+//cout<<"the value of b is "<<setw(4)<<b<<endl;
+//cout<<"the value of c is "<<setw(4)<<c<<endl;
+//}
+//# include<iostream>
+//using namespace std;
+//main(){
+// control statement
+# include <iostream>
+//using namespace std;
+//main(){
+//	int age;
+//	cout<<"enter the age "<<endl;
+//	cin>>age;
+//	if (age <18){
+//		cout<<"you are not eligible to party";
+//		
+//	}
+//	else if (age==18){
+//		cout<<"you are eligibe to party as a kid"<<endl;
+//		
+//	}
+//	else {
+//		cout<<"your most welcome "<<endl;
+//	}
+//switch(age)
+//{
+//	case 18:
+//		cout<<"you  are 18"<<endl;
+//		break;
+//	case 22:
+//		cout<<"you  are 22"<<endl;
+//		break;
+//		
+//	case 2:
+//	cout<<"you are 2"<<endl;
+//	break;
+//	default:
+//		cout<<"no special cases"<<endl;
+//		break;
+//		
+//	
+//		
+//		
+//}
+//cout<<"done with switch cases"<<endl;	
+//}
+// loop 
+// for loop initialization condition and updation
+//# include<iostream>
+//using namespace std;
+//main() {
+	
+//	for(int i=0;i<40;i++)
+//	{
+//		cout<<i<<endl;
+//		
+//	}
+//for (int i=0;34<40;i++){
+//	cout<<i<<endl;
+//	
+//}
+// while loop
+//int i=4;
+//while(i<=40){
+//	cout<<i<<endl;
+//	i++;
+//while(true){
+//	cout<<i<<endl;
+//	i++;
+//}
+// do- while loop
+//int i=4;
+//
+//do{
+//	cout<<i<<endl;
+//	i++;	
+//}while (i<=40);
+// break and continue
+//for(int i=0;i<40;i++){
+//	if (i==20){
+//		break;
+//	}
+//	cout<<i<<endl;
+//}
+//}
+// pointers :: data type that holds the address of other data types
+//# include<iostream>
+//using namespace std;
+//main(){
+//	int a=3;
+//	int* b=&a;
+//	//& ---> address of operator
+//	//*---> dereference of operator
+//	
+//	cout<<"address of a is "<<&a<<endl;
+//	cout<<"address of a is "<<b<<endl;
+//	cout<<"the value at address b is "<<*b<<endl;
+//	int** c=&b;
+//	cout<<"the address of b is"<<&b<<endl;
+//	cout<<"the address of b is "<<c<<endl;
+//	cout<<"the value at address c is "<<**c<<endl;
+// array 
+//int marks[4]={23,45,65,67};
+//cout<<marks[0]<<endl;
+//cout<<marks[1]<<endl;
+//marks[2]=49;
+//cout<<marks[2]<<endl;
+//cout<<marks[3]<<endl;
+//for(int i =0;i<4;i++){
+//	cout<<"the value of marks" <<i<< "is "<<marks[i]<<endl;
+//int * p=marks;
+//
+//cout<<"the value of marks[0] is "<<*p<<endl;
+//cout<<"the value of marks[1] is"<<*(p+1)<<endl;
+//cout<<"the value of marks[2] is "<<*(p+2)<<endl;
+//cout<<"the value of marks[3] is"<<*(p+3)<<endl;
+//cout<<*(p);
+//cout<<*(++p);	
+//}
+// structure
+//#include <iostream>
+//using namespace std;
+
+
+//typedef	struct employee
+//	{
+//	
+//	int id;
+//	char favchar;
+//	float salary;
+//}ep;
+//	union money{
+//		int rice;
+//		char car;
+//		float pounds;
+//		
+//	};
+//
+//	
+//
+//main(){
+//      ep harry;
+//      union money m1;
+//      m1.rice=34;
+//      m1.car='c';
+//	harry.id=2;
+//	harry.favchar ='t';
+//	harry.salary=1200000;
+//	cout<<harry.salary<<endl;
+//	cout<<harry.favchar<<endl;
+//	cout<<harry.id<<endl;
+//	cout<<m1.rice<<endl;
+//	cout<<m1.car<<endl;
+//	enum meal{breakfast,lunch,dinne};
+//	meal m2=lunch;
+//	cout<<m2;	
+//}
+// function 
+# include <iostream>
+using namespace std;
+//int sum(int ,int );
+//void greet(void);
+//	
+//
+//main(){
+//	int num1,num2,z;
+//	cout<<"enter the  first number "<<endl;
+//	cin>>num1;
+//	cout<<"enter the second number"<<endl;
+//	cin>>num2;
+//	z=sum(num1,num2);
+//	cout<<"the sum of number is "<<z<<endl;
+//	greet();
+//
+//}
+//int sum(int a,int b){
+//	int c=a+b;
+//	return c;
+//	
+//}
+//void greet(){
+//	cout<<"hello good morning";
+// call by value and call by reference
+//void swap (int a,int b){
+//	int temp=a;
+//	a=b;                      
+//	b=temp;  
+                  
+//void pointer(int* a,int*b){
+//nt temp=*a;
+//a=*b;
+//b=temp;
+		 
+//
+//void swapreference(int &a,int &b){
+//	int temp=a;
+//	b=a;
+//	b=temp;
+//}
+
+//main(){
+//	int a=4,b=5;
+//	cout<<"the value of a is"<<a<<"the value of b is "<<b<<endl;
+//	swap(a,b);
+//	cout<<"the value of a is"<<a<<"the value of b is "<<b<<endl;
+//	int x=3,y=2;
+	//cout<<"the value of a is "<<a<<"the value of b is"<<b<<endl;
+	//appointer(&x,&y);
+	//ut<<"the value of x is"<<x<<"alue of y is"<<endl;
+//	swapreference(x,y);
+//	cout<<'the value of x is'<<x<<'the value of y is'<<y<<endl;			
+//}
+//# include <iostream>
+//using namespace std;
+//inline int product(int a,int b){
+//	static int c=0; // this executes only one
+//	c=c+1; // next time this function is run ,the value of c will be retained
+//	return a*b+c;	
+//}
+//main(){
+//	int a,b;
+//	cout<<"enter the value of a and b"<<endl;
+//	cin>>a>>b;
+//	
+//	cout<<"the product of a and b is"<<product(a,b)<<endl;
+//	cout<<"the product of a and b is"<<product(a,b)<<endl;
+//	cout<<"the product of a and b is"<<product(a,b)<<endl;
+//	cout<<"the product of a and b is"<<product(a,b)<<endl;
+//	cout<<"the product of a and b is"<<product(a,b)<<endl;
+//	cout<<"the product of a and b is"<<product(a,b)<<endl;
+//	cout<<"the product of a and b is"<<product(a,b)<<endl;
+//	
+//}
+// recursion and recursive function 
+// factorial of number
+# include <iostream>
+using namespace std;
+//int factorial (int n){
+//	return n*factorial(n-1);	
+//}
+//main(){
+//	int a;
+//	cout<<"enter the value of a"<<endl;
+//	cin>>a;
+//	cout<<"the factorial of a is "<<factorial(a)<<endl;	
+//}
+// fibonacci series
+//int fib(int n){
+//	if (n==1){
+//		return 1;
+//		
+//	}
+//	return fib(n-2)+fib(n-1);
+//}
+//main(){
+//	int a;
+//	cout<<"the value a is"<<endl;
+//	cin>>a;
+//	cout<<"the series is "<<fib(a)<<endl;
+//}
+
+// function overloaded
+int sum(int a,int b){
+	cout<<"using function with 2 argument"<<endl;
+	return a+b;
+	
+}
+int sum(int a,int b,int c){
+	cout<<"using function with 3 argument"<<endl;
+	return a+b+c;	
+}
+main(){
+	cout<<"the sum is "<<sum(3,6)<<endl;
+	cout<<"the sum is "<<sum(3,5,7)<<endl;
+	
+}
+
+
+
+
+             
+
+
+
+
+
+
+
+
+
+
+
+
